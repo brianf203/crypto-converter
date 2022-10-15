4da7484fae3c86e34febf75f7be8e8c1a2103973
brianf203 <briancf203@gmail.com>
2022-10-15 16:41:05 -0400

    added crypto api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+var axios = require("axios");
+
 function App() {
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
+
+  var tickerOne = "ETH";
+  var tickerTwo = "BTC";
+
+  var pathOne = "https://rest.coinapi.io/v1/exchangerate/" + tickerOne + "/USD";
+  var pathTwo = "https://rest.coinapi.io/v1/exchangerate/" + tickerTwo + "/USD";
+
+  useEffect(() => {
+    const expensesListResp = async () => {
+      await axios.get(pathOne, { headers: {"X-CoinAPI-Key": "A576F188-FCEA-4546-A345-0059B152707F"} })
+      .then(
+        resp => setCurrencyOptions(resp.data.rate)
+      )
+    }
+    expensesListResp();
+  }, []);
+
   return (
     <>
       <h1>Convert</h1>
