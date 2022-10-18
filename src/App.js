1071a5fee4fc6accc3e8421532bc3f3a02d04bfc
brianf203 <briancf203@gmail.com>
2022-10-18 16:40:50 -0400

    added conversion

--- src/App.js
+++ src/App.js
@@ -6,31 +6,81 @@ var axios = require("axios");
 
 function App() {
 
-  const [currencyOptions, setCurrencyOptions] = useState([])
-  console.log(currencyOptions)
-
-  var tickerOne = "ETH";
-  var tickerTwo = "BTC";
-
-  var pathOne = "https://rest.coinapi.io/v1/exchangerate/" + tickerOne + "/USD";
-  var pathTwo = "https://rest.coinapi.io/v1/exchangerate/" + tickerTwo + "/USD";
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [fromPrice, setFromPrice] = useState()
+  const [toPrice, setToPrice] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  const tickers = ["BTC", "ETH", "BNB", "DOGE"]
+  
+  let toAmount, fromAmount
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  }
+  else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
 
   useEffect(() => {
-    const expensesListResp = async () => {
-      await axios.get(pathOne, { headers: {"X-CoinAPI-Key": "A576F188-FCEA-4546-A345-0059B152707F"} })
-      .then(
-        resp => setCurrencyOptions(resp.data.rate)
-      )
+    fromAmount = 1;
+    setFromCurrency("BTC")
+    setToCurrency("ETH")
+    setExchangeRate(1)
+  }, [])
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+
+      console.log("FROM CURRENCY: " + fromCurrency)
+      console.log("TO CURRENCY: " + toCurrency)
+
+      axios.get("https://rest.coinapi.io/v1/exchangerate/" + fromCurrency + "/USD", { headers: {"X-CoinAPI-Key": "D53E1FC5-E2E7-4126-A3E7-98BB55B8D634"} })
+        .then(function(resp) {
+          setFromPrice(resp.data.rate)
+          console.log(fromCurrency + " price: " + fromPrice + "")
+      });
+      axios.get("https://rest.coinapi.io/v1/exchangerate/" + toCurrency + "/USD", { headers: {"X-CoinAPI-Key": "D53E1FC5-E2E7-4126-A3E7-98BB55B8D634"} })
+        .then(function(resp) {
+          setToPrice(resp.data.rate)
+          console.log(toCurrency + " price: " + toPrice + "")
+      });
+
+      setExchangeRate(fromPrice / toPrice)
     }
-    expensesListResp();
-  }, []);
+  }, [fromCurrency, toCurrency])
+
+  function handFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
 
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow />
+      <CurrencyRow 
+        tickers={tickers}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
+        onChangeAmount={handFromAmountChange}
+        amount={fromAmount}
+      />
       <div className="equals">=</div>
-      <CurrencyRow />
+      <CurrencyRow 
+        tickers={tickers}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={e => setToCurrency(e.target.value)}
+        onChangeAmount={handToAmountChange}
+        amount={toAmount}
+      />
     </>
   );
 }
